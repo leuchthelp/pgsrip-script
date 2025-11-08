8fb8a5575dbe68c8fee862358c1ecf34479bd6d2
leucht <gfpinthehouse@gmail.com>
2025-11-08 23:43:09 +0100

    Little clean, Add logging

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from pathlib import Path
-from dataclasses import dataclass
 from multiprocessing import Pool
+import logging
 import subprocess
 import tqdm
 import os
 
-@dataclass
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def run(task):
     
+    logger.info(bcolors.OKCYAN + f"current on: {task}" + bcolors.ENDC)
     p = subprocess.run(task, check=True, text=True, capture_output=True)
-    print(p.stderr)
-    print(p.stdout)
-
+    logger.debug(p.stderr)
+    logger.debug(p.stdout)
 
 
 def main():
-
+    
+    ### SETUP HERE ###
     os.environ["TESSDATA_PREFIX"] = "C:\\Users\\leucht\\Videos\\tessdata_best\\" # Path tessdata_best as instructed by pgsrip
     root = Path("Y:\\jellyfin\\shows\\") # Path to directory of .mkv files that you want to convert the subtitles of
 
@@ -35,18 +35,18 @@ def main():
 
     for path in root.rglob("*"):
 
-        #print(path)
+        logger.debug(path)
         if not path.is_dir() and ".mkv" in path.name: 
 
             convertables.append(f"pgsrip \"{path.absolute()}\"")
 
-    #print(convertables)
+    logger.debug(convertables)
 
     pool = Pool()
     for _ in tqdm.tqdm(pool.imap_unordered(run, convertables), total=len(convertables)):
         pass
 
-    print(bcolors.OKGREEN + "Finished" + bcolors.ENDC)
+    logger.info(bcolors.OKGREEN + "Finished" + bcolors.ENDC)
 
 
 if __name__=="__main__":
